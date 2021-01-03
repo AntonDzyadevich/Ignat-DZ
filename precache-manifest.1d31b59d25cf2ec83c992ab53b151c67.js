69330e32c022acf87dabecbfd5f05e49eda444a1
AntonDzyadevich <antonbilder86@gmail.com>
2021-01-03 20:14:10 +0300

    Updates

---
+++ precache-manifest.1d31b59d25cf2ec83c992ab53b151c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101c3332742bc1e5c279b08b2a3280e9",
+    "url": "/Ignat-DZ/index.html"
+  },
+  {
+    "revision": "fd6daf1dc9ff71f9fb9a",
+    "url": "/Ignat-DZ/static/css/main.65d81efb.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/Ignat-DZ/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ignat-DZ/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6daf1dc9ff71f9fb9a",
+    "url": "/Ignat-DZ/static/js/main.6283ace7.chunk.js"
+  },
+  {
+    "revision": "8e52e0ca970c85a51a4e",
+    "url": "/Ignat-DZ/static/js/runtime-main.d2365cdb.js"
+  }
+]);
